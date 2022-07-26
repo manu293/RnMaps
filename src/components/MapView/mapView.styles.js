a72056c15ee826b1db40c4fb724a4a44a9cd2e99
manu293 <manojsm293@gmail.com>
2022-07-26 15:52:04 +0530

    feat: Add MapView Component And Styles

---
+++ src/components/MapView/mapView.styles.js
@@ -0,0 +1,10 @@
+// imports
+import {StyleSheet} from 'react-native';
+
+const MapViewStyles = StyleSheet.create({
+  mapView: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default MapViewStyles;
