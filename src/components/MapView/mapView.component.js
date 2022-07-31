2be00ee2081289545050d3ed4776c7203d8fbc4d
manu293 <manojsm293@gmail.com>
2022-07-31 13:00:58 +0530

    chore: Commented Map View Component

--- src/components/MapView/mapView.component.js
+++ src/components/MapView/mapView.component.js
@@ -1,26 +1,26 @@
-// imports
-import React from 'react';
-import {Platform} from 'react-native';
-import MapView, {PROVIDER_GOOGLE, PROVIDER_DEFAULT} from 'react-native-maps';
+// // imports
+// import React from 'react';
+// import {Platform} from 'react-native';
+// import MapView, {PROVIDER_GOOGLE, PROVIDER_DEFAULT} from 'react-native-maps';
 
-// local imports
-import MapViewStyles from './mapView.styles';
+// // local imports
+// import MapViewStyles from './mapView.styles';
 
-const MapViewComponent = () => {
-  const defaultProvider =
-    Platform.OS === 'ios' ? PROVIDER_DEFAULT : PROVIDER_GOOGLE;
-  return (
-    <MapView
-      provider={defaultProvider}
-      style={MapViewStyles.mapView}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }}
-    />
-  );
-};
+// const MapViewComponent = () => {
+//   const defaultProvider =
+//     Platform.OS === 'ios' ? PROVIDER_DEFAULT : PROVIDER_GOOGLE;
+//   return (
+//     <MapView
+//       provider={defaultProvider}
+//       style={MapViewStyles.mapView}
+//       initialRegion={{
+//         latitude: 37.78825,
+//         longitude: -122.4324,
+//         latitudeDelta: 0.015,
+//         longitudeDelta: 0.0121,
+//       }}
+//     />
+//   );
+// };
 
-export default MapViewComponent;
+// export default MapViewComponent;
